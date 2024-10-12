e648d1e4a7d9b5fb4584b394016a21576298c6b2
Alex Grevtsev <aigrevtcev@gmail.com>
2024-10-12 13:17:29 +0300

    fix

--- lab1/vars.php
+++ lab1/vars.php
@@ -1,17 +1,28 @@
 <?php
-
 $name = 'Алекс';
 $age = 20;
-
-echo "Меня зовут: $name<br>";
-echo "Мне $age лет<br>";
-
-echo "Тип переменной \$name: " . gettype($name) . "<br>";
-echo "Тип переменной \$age: " . gettype($age) . "<br>";
-
-unset($name, $age);
-
-echo "Переменная \$name после удаления: " . (isset($name) ? $name : 'переменная удалена') . "<br>";
-echo "Переменная \$age после удаления: " . (isset($age) ? $age : 'переменная удалена') . "<br>";
-
 ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Переменные и вывод</title>
+</head>
+<body>
+	<h1>Переменные и вывод</h1>
+	<?php
+    echo "Меня зовут: $name<br>";
+    echo "Мне $age лет<br>";
+    
+    echo "Тип переменной \$name: " . gettype($name) . "<br>";
+    echo "Тип переменной \$age: " . gettype($age) . "<br>";
+    
+    unset($name, $age);
+    
+    echo "Переменная \$name после удаления: " . (isset($name) ? $name : 'переменная удалена') . "<br>";
+    echo "Переменная \$age после удаления: " . (isset($age) ? $age : 'переменная удалена') . "<br>";
+	?> 
+</body>
+</html>
