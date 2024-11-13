aa9da9ff855b9b9aae1cf19e8295486b5a0072f4
Alex Grevtsev <aigrevtcev@gmail.com>
2024-11-13 18:13:11 +0300

    upd

--- lab6/session/visited.inc.php
+++ lab6/session/visited.inc.php
@@ -1,6 +1,11 @@
 <?php
 declare(strict_types=1);
 
+// Устанавливаем параметры для сессий
+ini_set("session.use_only_cookies", "0");
+ini_set("session.use_trans_sid", "1");
+session_start();
+
 function displayVisitedPages(): void {
     if (isset($_SESSION['visited_pages']) && count($_SESSION['visited_pages']) > 0) {
         echo '<h2>Список посещённых страниц</h2>';
@@ -13,5 +18,7 @@ function displayVisitedPages(): void {
         echo '<p>Нет посещённых страниц.</p>';
     }
 }
+
+// Вызов функции для отображения посещённых страниц
 displayVisitedPages();
 ?>
