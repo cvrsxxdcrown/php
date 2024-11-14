cba98e64db13b6006d0f2b88ababddde344cccbc
Alex Grevtsev <aigrevtcev@gmail.com>
2024-11-14 16:12:20 +0300

    fix

--- lab8/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'f1038297_login_db');
-define('DB_PASS', '21021976Ok@');
-define('DB_NAME', 'f1038297_login_db');
-define('DB_CHARSET', 'utf8mb4');
-?>
